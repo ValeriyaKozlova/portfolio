4b754e574c3906299dfb481328c3c69878556ac8
ValeriyaKozlova <kozlova.valeriya.v@gmail.com>
2020-08-03 18:58:41 +0200

    education mobile almost done

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,30 +4,32 @@ import translate from "../i18n/translate";
 const Education = () => {
   return (
     <section className="col-12 education">
-      <h1 className="d-none d-lg-block">{translate("heading-education")}</h1>
-      <div className="message">
+      <h1 className="col-12 d-none d-lg-block">{translate("heading-education")}</h1>
+      <div className="col-12 col-lg-6  offset-lg-3 message">
         <p id="message">{translate("message")}</p>
       </div>
-      <div className="profPath">
-        <div className="workContainer lightBox firstBox">
+      <div className="col-12 container profPath">
+        <div className="row">
+        <div className="col-12 col-lg-3 workContainer lightBox firstBox">
           <p>2019</p>
           <p>{translate('description-technofuture')} </p>
           <p>{translate("country")}</p>
         </div>
-        <div className="workContainer darkBox secondBox">
+        <div className="col-12 col-lg-3 workContainer darkBox secondBox">
           <p>{translate("date-becode")}</p>
           <p>{translate("description-becode")}</p>
           <p>{translate("country-city")}</p>
         </div>
-        <div className="workContainer lightBox thirdBox">
+        <div className="col-12 col-lg-3 workContainer lightBox thirdBox">
           <p>{translate('date-blooming')} </p>
           <p>
             {translate('description-blooming')}
           </p>
           <p>{translate("country")}</p>
         </div>
-        <div className="workContainer darkBox next">
+        <div className="col-12 col-lg-3 workContainer darkBox next">
           <h2>What's next?..</h2>
+        </div>
         </div>
       </div>
     </section>

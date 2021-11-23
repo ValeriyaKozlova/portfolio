eea7ebdfcdddc9e75dc73c61a5954792c2bb9c7b
ValeriyaKozlova <kozlova.valeriya.v@gmail.com>
2021-11-23 22:11:34 +0100

    htaccess added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -11,7 +10,6 @@ ReactDOM.render(
     <App />
   </BrowserRouter>,
   document.getElementById('root')
-
 );
 
 // If you want your app to work offline and load faster, you can change

0e04cdaf46fd8985d05863a04e14ed1910b7e3e8
ValeriyaKozlova <kozlova.valeriya.v@gmail.com>
2021-11-24 00:13:20 +0100

    text edited

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,14 @@ import translate from "../i18n/translate";
 
 const About = () => {
 
+  console.log(translate`message`)
   return (
     <section className="aboutContainer">
       <div className="container" id="about">
         <div className="row">
           <h2 className="col-12 d-none d-lg-block">{translate("nav-about")}</h2>
           <div className="col-12 col-lg-6 message">
-            <p id="message">{translate("message")}</p>
+            <p id="message">{translate`message`}</p>
           </div>
           <div className="col-12 col-lg-6 skills">
             <ul>
